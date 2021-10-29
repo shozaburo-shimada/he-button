622b59dd2decd2f89d34e62fb17cf1769e386982
Shozaburo Shimada <shozaburo@vivita.co>
2021-10-29 15:38:57 +0900

    initial update

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+   functions.logger.info("Hello logs!", {structuredData: true});
+   response.send("Hello from Firebase!");
+});
+
+
+exports.helloOnCall = functions.https.onCall((data, context) => {
+   return "Hello OnCall!";
+});
